f22db2e71e50c398af66436f83d9a8ec5bd0f34d
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-12-04 09:14:07 -0500

    Extracted some logic into functions

--- scripts/yolo_detect.py
+++ scripts/yolo_detect.py
@@ -1,16 +1,15 @@
 #!/home/alex/catkin_ws/src/Grace/yolovenv/bin/python
 import os
-from typing import Final, List, Union
+from typing import Final, List, Tuple, Union
 import rospy
 import ros_numpy
 import numpy as np 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PointStamped
 from ultralytics import YOLO
 import requests
 from ultralytics.engine.results import Boxes, Results
 import cv2
-import math
 
 # Set the environment variable
 os.environ['YOLO_VERBOSE'] = 'False'
@@ -33,7 +32,9 @@ class YoloDetect:
         # Subscribers
         self.rgb_image_sub = rospy.Subscriber(name="/camera/rgb/image_raw", data_class=Image, callback=self.rgb_image_callback)
         self.depth_image_sub = rospy.Subscriber(name="/camera/depth/image_raw", data_class=Image, callback=self.depth_image_callback)
-        self.transformed_point_sub = rospy.Subscriber(name="transform_node/base_link_frame_point", data_class=PointStamped, callback=self.transformed_point_callback)
+        self.transformed_point_sub = rospy.Subscriber(name="/transform_node/base_link_frame_point", data_class=PointStamped, callback=self.transformed_point_callback)
+        # self.camera_info_sub = rospy.Subscriber(name="/camera/depth/camera_info", data_class=CameraInfo)
+
 
         MODEL_NAME: Final = 'yolo11s.pt'
         # Download the YOLO model to the grace folder
@@ -60,6 +61,8 @@ class YoloDetect:
         self.latest_rgb_image = None
         self.latest_depth_image = None
         self.transformed_point = None
+        self.camera_info: CameraInfo = rospy.wait_for_message(topic="/camera/depth/camera_info", topic_type=CameraInfo) # type: ignore
+
 
         # Timer to call the detection function at a fixed interval (4 times a second)
         self.timer = rospy.Timer(period=rospy.Duration(secs=DETECTION_INTERVAL), callback=self.detect_objects) # type: ignore
@@ -73,6 +76,11 @@ class YoloDetect:
     def transformed_point_callback(self, point) -> None:
         self.transformed_point = point
 
+    def __call__(self) -> None:
+        """Make it so that you can do `YoloDetect()` and it starts the class.
+        """
+        self.run()
+    
     def detect_objects(self, event: rospy.timer.TimerEvent) -> None:
         """Callback function to detect objects on RGB image"""
         # if self.latest_rgb_image is not None and self.latest_depth_image is not None:
@@ -103,45 +111,62 @@ class YoloDetect:
                     det_annotated = cv2.putText(det_annotated, label, (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                     # * https://towardsdatascience.com/camera-intrinsic-matrix-with-example-in-python-d79bf2478c12
                     # https://learnopencv.com/monocular-slam-in-python/
-                    point: PointStamped = PointStamped()
-                    point.header.stamp = rospy.Time.now()
-                    point.header.frame_id = "camera_link"
-                    # cx: float = (x1 + x2) / 2 # Mean of x
-                    # cy: float = (y1 + y2) / 2 # Mean of y
-                    # Obtain the parameters of the camera by doing the following:
-                    # Run the camera, then run rostopic echo /camera/rgb/camera_info
-                    # K is the intrinsics matrix. Per the header file definition of CameriaInfo,
+                    top_left_corner: tuple[int, int] = (x1, y1)
+                    bottom_right_corner: tuple[int, int] = (x2, y2)
 
-                    ## Intrinsic camera matrix for the raw (distorted) images.
-                    #     [fx  0 cx]
-                    # K = [ 0 fy cy]
-                    #     [ 0  0  1]
-                    # Projects 3D points in the camera coordinate frame to 2D pixel
-                    # coordinates using the focal lengths (fx, fy) and principal point
-                    # (cx, cy).
-
-                    KINECT_HEIGHT = 480
-                    KINECT_WIDTH = 640
-                    fx = 554.25 # Kinect's focal length
-                    fy = 554.25 # Kinect's focal length
-                    cx = 320.5 # Principal point x-coordinate
-                    cy = 240.5 # Principal point y-coordinate
-
-                    x_coordinate: float = (x1 + x2) / 2
-                    y_coordinate: float = (y1 + y2) / 2
-
-                    # Unproject x and y to camera coordinates
-                    x_camera: float = depth * (x_coordinate - cx) / fx
-                    y_camera: float = depth * (y_coordinate - cy) / fy
-                    z_camera: float = depth
-
-                    point.point.x = x_camera
-                    point.point.y = y_camera
-                    point.point.z = z_camera
+                    point: PointStamped = self.image_coordinates_to_camera_frame(top_left_corner=top_left_corner, bottom_right_corner=bottom_right_corner, depth=depth)
+                    point = self.camera_to_world_coordinates(point)
                     
-                    self.semantic_objects_pub.publish(point)
-            self.detection_image_pub.publish(ros_numpy.msgify(Image, det_annotated, encoding="rgb8"))  # publish the annotated image
+                    self.semantic_objects_pub.publish(point) # Publish the camera frame point
+            self.detection_image_pub.publish(ros_numpy.msgify(Image, det_annotated, encoding="rgb8")) 
 
+    def image_coordinates_to_camera_frame(self, top_left_corner: Tuple[int, int], bottom_right_corner: Tuple[int, int], depth) -> PointStamped:
+        point: PointStamped = PointStamped()
+        point.header.stamp = rospy.Time.now()
+        point.header.frame_id = "camera_link"
+        # Obtain the parameters of the camera by doing the following:
+        # Run the camera, then run rostopic echo /camera/rgb/camera_info
+        # K is the intrinsics matrix. Per the header file definition of CameriaInfo,
+
+        ## Intrinsic camera matrix for the raw (distorted) images.
+        #     [fx  0 cx]
+        # K = [ 0 fy cy]
+        #     [ 0  0  1]
+        # Projects 3D points in the camera coordinate frame to 2D pixel
+        # coordinates using the focal lengths (fx, fy) and principal point
+        # (cx, cy).
+        KINECT_HEIGHT = 480
+        KINECT_WIDTH = 640
+        fx: float = self.camera_info.K[0] # Kinect's focal length
+        fy: float = self.camera_info.K[4] # Kinect's focal length
+        cx: float = self.camera_info.K[2] # Principal point x-coordinate
+        cy: float = self.camera_info.K[5] # Principal point y-coordinate
+
+        x_coordinate: float = (top_left_corner[0] + bottom_right_corner[0]) / 2
+        y_coordinate: float = (top_left_corner[1] + bottom_right_corner[1]) / 2
+
+        # Unproject x and y to camera coordinates
+        x_camera: float = depth * (x_coordinate - cx) / fx
+        y_camera: float = depth * (y_coordinate - cy) / fy
+        z_camera: float = depth
+
+        point.point.x = x_camera
+        point.point.y = y_camera
+        point.point.z = z_camera
+        return point
+
+    def camera_to_world_coordinates(self, camera_coordinates: PointStamped) -> PointStamped:
+        point: PointStamped = PointStamped()
+        point.header.stamp = rospy.Time.now()
+        point.header.frame_id = "map" # This COULD be base_link if world doesn't work
+
+        rotation_matrix: np.ndarray = np.reshape(self.camera_info.R, (3, 3))
+        world_point = np.dot(rotation_matrix, [camera_coordinates.point.x, camera_coordinates.point.y, camera_coordinates.point.z])
+        
+        point.point.x = world_point[0]
+        point.point.y = world_point[1]
+        point.point.z = world_point[2]
+        return point
     def run(self) -> None:
         rospy.spin()
 
